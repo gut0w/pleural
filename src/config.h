e9a68cad25df06d122c531bcb49ad675ffc3c4a1
Renato <renato@poli.ufrj.br>
2009-06-09 14:36:45 -0300

    src adicionado

---
+++ src/config.h
@@ -0,0 +1,24 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WEB_SERVER "https://www.gruyere.lps.ufrj.br/~pleural/dev"
+#define WEB_PATH_DOENTES_NOVOS_PACIENTE_ADICIONADO_XML "https://www.gruyere.lps.ufrj.br/~pleural/dev/doentesNovos/xml/pacienteAdicionado.xml"
+#define WEB_PATH_FILE_DOENTES_NOVOS_BUSCA_XSL "https://www.gruyere.lps.ufrj.br/~pleural/dev/doentesNovos/xml/busca.xsl"
+#define WEB_PATH_FILE_DOENTES_NOVOS_LISTA_PACIENTES_XSL "https://www.gruyere.lps.ufrj.br/~pleural/dev/doentesNovos/xml/listaPacientes.xsl"
+#define WEB_PATH_FILE_DOENTES_NOVOS_APOS_BUSCA_EDITA_PACIENTE_CGI "https://www.gruyere.lps.ufrj.br/~pleural/dev/doentesNovos/cgi-bin/editaPaciente.cgi?paciente="
+#define WEB_PATH_FILE_DOENTES_NOVOS_APOS_BUSCA_REMOVE_PACIENTE_CGI "https://www.gruyere.lps.ufrj.br/~pleural/dev/doentesNovos/cgi-bin/motivoRemove.cgi?paciente="
+#define WEB_PATH_FILE_DOENTES_NOVOS_REMOVE_PACIENTE_CGI "https://www.gruyere.lps.ufrj.br/~pleural/dev/doentesNovos/cgi-bin/removePatient.cgi"
+#define WEB_PATH_DOENTES_NOVOS_INICIO "https://www.gruyere.lps.ufrj.br/~pleural/dev/doentesNovos/inicio.html"
+#define WEB_PATH_FILE_MORRYS_INDEX_SISTEMA_2 "https://www.gruyere.lps.ufrj.br/~pleural/dev/indexSistema2.html"
+#define WEB_PATH_FILE_DOENTES_NOVOS_INDEX_SISTEMA_2 "https://www.gruyere.lps.ufrj.br/~pleural/dev/doentesNovos/indexSistema2.html"
+#define WEB_PATH_FILE_DOENTES_NOVOS_PACIENTE_ADICIONADO_XSL "https://www.gruyere.lps.ufrj.br/~pleural/dev/doentesNovos/xml/pacienteAdicionado.xsl"
+#define WEB_PATH_FILE_DOENTES_NOVOS_APOS_BUSCA_VER_PACIENTE_CGI "https://www.gruyere.lps.ufrj.br/~pleural/dev/doentesNovos/cgi-bin/verPaciente.cgi?paciente="
+#define LOCAL_PATH_FILE_DOENTES_NOVOS_PACIENTE_ADICIONADO_XML "../doentesNovos/xml/pacienteAdicionado.xml"
+#define LOCAL_PATH_FILE_DOENTES_NOVOS_BUSCA_XSL "../doentesNovos/xml/busca.xsl"
+#define LOCAL_PATH_FILE_DOENTES_NOVOS_PACIENTES_REMOVIDOS_XML "../doentesNovos/xml/removed.xml"
+#define LOCAL_PATH_FILE_DOENTES_NOVOS_HISTORICO_XML "../doentesNovos/xml/historic.xml"
+#define LOCAL_PATH_FILE_DOENTES_NOVOS_PACIENTE_ADICIONADO_TEMP_XML "../doentesNovos/xml/pacienteAdicionadoTemp.xml"
+#define LOCAL_PATH_FILE_DOENTES_NOVOS_BACKUP "../doentesNovos/backup/"
+#define LOCAL_PATH_FILE_DOENTES_NOVOS_VER_PACIENTE_XSL " "
+
+#endif
